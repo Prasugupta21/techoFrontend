9ed97478c2ffc5dc4d4c6c841f58f13c8a49bb77
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2025-01-17 11:25:03 +0530

    Home Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path='/' element={<Header/>}/>
+      <Route path='/' element={<HomePage/>}/>
     </Routes>
   
     </>
